4566ac843712077fbeebcae8345aae62f051ceb5
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2024-06-19 16:18:54 -0300

    bootstrap the app backend with supabase

---
+++ database/seed.js
@@ -0,0 +1,26 @@
+/* eslint-env node */
+
+import { fakerEN_US as faker } from '@faker-js/faker';
+import { createClient } from '@supabase/supabase-js';
+
+// Create a single supabase client for interacting with your database
+export const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY);
+
+const seedProjects = async (numEntries) => {
+    const projects = [];
+
+    for (let i = 0; i <= numEntries; i++) {
+        const name = faker.lorem.words(3);
+
+        projects.push({
+            name: faker.lorem.words(3),
+            slug: name.toLocaleLowerCase().replace(/ /g, '-'),
+            status: faker.helpers.arrayElement(['in-progress', 'completed']),
+            collaborators: faker.helpers.arrayElements([1, 2, 3])
+        });
+    }
+
+    await supabase.from('projects').insert(projects);
+};
+
+await seedProjects(10);
