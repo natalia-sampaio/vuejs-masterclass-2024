bad9a2da1d7b92cdb73740a741a1981265487d01
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2024-08-07 18:10:46 -0300

    finished masterclass part 3

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -3,26 +3,25 @@ import { supabase } from '@/lib/supabaseClient';
 import { ref } from 'vue';
 import type { Tables } from 'database/types';
 
-const projects = ref<Tables<'projects'>[] | null>(null);
+const tasks = ref<Tables<'tasks'>[] | null>(null);
 
-//Imediatly Invoked Function Expression
 (async () => {
-    const { data, error } = await supabase.from('projects').select();
+    const { data, error } = await supabase.from('tasks').select();
 
     if (error) console.log(error);
 
-    projects.value = data;
+    tasks.value = data;
 
-    console.log(projects.value);
+    console.log(tasks.value);
 })();
 </script>
 <template>
     <div>
-        <h1>Projects Page</h1>
+        <h1>Tasks Page</h1>
         <RouterLink to="/">Go to home</RouterLink>
         <ul>
-            <li v-for="project in projects" :key="project.id">
-                {{ project.name }}
+            <li v-for="task in tasks" :key="task.id">
+                {{ task.name }}
             </li>
         </ul>
     </div>
